1de7c98e5a8df715ab603d3ed97f08e44ff0b23c
Viren Patil <viren.p2000@gmail.com>
2025-07-25 11:53:54 -0700

    Update: Firebase keys in .env

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC0ah02GSaZ7hszanROdZCHzYwmAzmpcGg",
-  authDomain: "movie-watchlist-68b3c.firebaseapp.com",
-  projectId: "movie-watchlist-68b3c",
-  storageBucket: "movie-watchlist-68b3c.firebasestorage.app",
-  messagingSenderId: "411985322031",
-  appId: "1:411985322031:web:d7019ad6f7f547a2f866da",
-  measurementId: "G-SH7ELTFHKK"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
